2a80fc816d52c6acf3365ade5c2c1ce4ec8a2080
iam-amanxz <husnulamanads@gmail.com>
2020-05-25 17:07:55 +0530

    .

--- src/main/java/com/ssdgroupc/app/TourApiApplication.java
+++ src/main/java/com/ssdgroupc/app/TourApiApplication.java
@@ -2,13 +2,12 @@ package com.ssdgroupc.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TourApiApplication extends SpringBootServletInitializer{
+public class TourApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TourApiApplication.class, args);
 	}
-	
+
 }
